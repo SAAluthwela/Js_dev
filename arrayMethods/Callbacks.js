82a7783f8dfdc335b7d50ee8b0896a536166f79f
SAAluthwela <supunakalanka.git@gmail.com>
2022-02-08 23:55:19 +0530

    callback example

--- arrayMethods/Callbacks.js
+++ arrayMethods/Callbacks.js
@@ -1,3 +1,9 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+const message = function () {
+  console.log("This message is shown after 3 seconds");
+};
+
+setTimeout(message, 3000);
