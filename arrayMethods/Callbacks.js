719b8255de3dc63bbb942d1e80be8b9216e57b66
SAAluthwela <supunakalanka.git@gmail.com>
2022-02-08 23:57:51 +0530

    anonymous function

--- arrayMethods/Callbacks.js
+++ arrayMethods/Callbacks.js
@@ -2,8 +2,15 @@
 //   console.log(i);
 // }
 
+//How to create call backs
 const message = function () {
   console.log("This message is shown after 3 seconds");
 };
 
 setTimeout(message, 3000);
+
+//Anonymous function
+
+setTimeout(function () {
+  console.log("This msg will show after 3000 milli seconds");
+}, 3000);
