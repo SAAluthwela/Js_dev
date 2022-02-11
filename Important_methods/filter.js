c4a255a35058f64ade17957fa0359897434f04b6
SAAluthwela <supunakalanka.git@gmail.com>
2022-02-11 23:58:00 +0530

    filter method uses

--- Important_methods/filter.js
+++ Important_methods/filter.js
@@ -1,3 +1,5 @@
+// <---------- basic filter() method ---------------->
+
 const words = [
   "spray",
   "limit",
@@ -12,7 +14,7 @@ const result = words.filter((word) => word.length > 6);
 console.log(result);
 // expected output: Array ["exuberant", "destruction", "present"]
 
-//Find all prime numbers in an array
+//< -----------------Find all prime numbers in an array ------------------>
 
 const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
@@ -26,3 +28,54 @@ function isPrime(num) {
 }
 
 console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+
+//searching in array
+
+let fruits = ["apple", "banana", "grapes", "mango", "orange"];
+
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+  return arr.filter(function (el) {
+    return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  });
+}
+
+console.log(filterItems(fruits, "ap")); // ['apple', 'grapes']
+
+//<---------  Affecting Initial Array (modifying, appending and deleting) ------->
+
+// Modifying each words
+let words = ["spray", "limit", "exuberant", "destruction", "elite", "present"];
+
+const modifiedWords = words.filter((word, index, arr) => {
+  arr[index + 1] += " extra";
+  return word.length < 6;
+});
+
+console.log(modifiedWords);
+// Notice there are three words below length 6, but since they've been modified one is returned
+// ["spray"]
+
+// Appending new words
+words = ["spray", "limit", "exuberant", "destruction", "elite", "present"];
+const appendedWords = words.filter((word, index, arr) => {
+  arr.push("new");
+  return word.length < 6;
+});
+
+console.log(appendedWords);
+// Only three fits the condition even though the `words` itself now has a lot more words with character length less than 6
+// ["spray" ,"limit" ,"elite"]
+
+// Deleting words
+words = ["spray", "limit", "exuberant", "destruction", "elite", "present"];
+const deleteWords = words.filter((word, index, arr) => {
+  arr.pop();
+  return word.length < 6;
+});
+
+console.log(deleteWords);
+// Notice 'elite' is not even obtained as its been popped off `words` before filter can even get there
+// ["spray" ,"limit"]
