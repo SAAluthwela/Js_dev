3a17f872f38dde26fd3f4545e6c4eb5f2c8eb387
SAAluthwela <supunakalanka.git@gmail.com>
2022-04-28 12:07:25 +0530

    learn

---
+++ arrayMethods/advanced.js
@@ -0,0 +1,17 @@
+//Arrow functions
+let a = 10;
+let b = 13;
+
+let func = (a) => {
+  if (a > 10) return "pass";
+};
+
+console.log(func(b));
+
+// let age = 30;
+
+// let output = age > 23 ? "Beer" : "Juice";
+
+// console.log(output);
+
+//Mapping
